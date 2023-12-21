6fcebbaee944baca87760a7d97942cafbc26b813
Tmax <71694025+Tmax9663@users.noreply.github.com>
2023-12-21 23:58:00 +0800

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const post = require("./routes/posts");
 const app = express();
 app.use(express.json());
 app.use(express.static(__dirname + "/public/"));
-app.use(cors());
 app.use("/api/post", post);
 app.get("/", (req, res) => {
   res.sendFile("index.html");
